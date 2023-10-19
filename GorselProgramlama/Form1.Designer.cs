2d9c1b04ba8dadc3b8e73049b9982ef04c895f5b
Semih AÇMALI <suhedasemihacmali@gmail.com>
2023-10-19 15:59:48 +0300

    Add files via upload

---
+++ GorselProgramlama/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace GorselProgramlama
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(225, 112);
+			this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(54, 22);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			this.label1.Visible = false;
+			this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.AntiqueWhite;
+			this.label2.Font = new System.Drawing.Font("Poplar Std", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(225, 164);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(40, 19);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "label2";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Orange;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.ClientSize = new System.Drawing.Size(496, 231);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Cursor = System.Windows.Forms.Cursors.Help;
+			this.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.Margin = new System.Windows.Forms.Padding(5);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ Hafta3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hafta3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_MouseClick(object sender, MouseEventArgs e)
+		{
+			string str1 = "Semih AÇMALI";
+			label1.Text = str1;
+		}
+
+		private void button1_MouseClick(object sender, MouseEventArgs e)
+		{
+			string str2 = textBox1.Text;
+			label1.Text = str2;
+		}
+
+		private void button2_MouseClick(object sender, MouseEventArgs e)
+		{
+			string username = textBox1.Text;
+			string password = textBox2.Text;
+
+			if(username == "DMYO")
+			{
+				// label1.Text = "Kullanıcı Adı Doğru!!";
+				if(password == "1234")
+				{
+					label1.Text = "Giriş Başarılı";
+				}
+				else
+				{
+					label1.Text = "Şifre Hatalı";
+				}
+			}
+			else
+			{
+				label1.Text = "Kullanıcı Adı Hatalı!!!";
+			}
+
+		}
+	}
+}
