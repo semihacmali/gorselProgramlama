6b4d7b26c4d93f4fc876d6dc225ab2e848c810ce
Semih AÇMALI <suhedasemihacmali@gmail.com>
2023-12-21 15:57:37 +0300

    Add files via upload

---
+++ CafeApp-Hafta10/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace CafeApp_Hafta10
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.pictureBox6 = new System.Windows.Forms.PictureBox();
+			this.pictureBox7 = new System.Windows.Forms.PictureBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::CafeApp_Hafta10.Resource1.logo;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(83, 52);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(-3, 304);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(62, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Kalan Tutar";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(0, 332);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(0, 13);
+			this.label2.TabIndex = 2;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox2.Image = global::CafeApp_Hafta10.Resource1.cheese;
+			this.pictureBox2.Location = new System.Drawing.Point(89, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(123, 117);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox2.TabIndex = 3;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(415, -4);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(35, 13);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Sepet";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(373, 12);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(103, 160);
+			this.listBox1.TabIndex = 10;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(387, 304);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 11;
+			this.button1.Text = "Satın Al";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox3.Image = global::CafeApp_Hafta10.Resource1.cupcake;
+			this.pictureBox3.Location = new System.Drawing.Point(218, 0);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(123, 117);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox3.TabIndex = 12;
+			this.pictureBox3.TabStop = false;
+			this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox4.Image = global::CafeApp_Hafta10.Resource1.hotdog;
+			this.pictureBox4.Location = new System.Drawing.Point(218, 123);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(123, 117);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox4.TabIndex = 14;
+			this.pictureBox4.TabStop = false;
+			this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox5.Image = global::CafeApp_Hafta10.Resource1.hamburger;
+			this.pictureBox5.Location = new System.Drawing.Point(89, 123);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(123, 117);
+			this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox5.TabIndex = 13;
+			this.pictureBox5.TabStop = false;
+			this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+			// 
+			// pictureBox6
+			// 
+			this.pictureBox6.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox6.Image = global::CafeApp_Hafta10.Resource1.pizza;
+			this.pictureBox6.Location = new System.Drawing.Point(218, 246);
+			this.pictureBox6.Name = "pictureBox6";
+			this.pictureBox6.Size = new System.Drawing.Size(123, 117);
+			this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox6.TabIndex = 16;
+			this.pictureBox6.TabStop = false;
+			this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+			// 
+			// pictureBox7
+			// 
+			this.pictureBox7.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.pictureBox7.Image = global::CafeApp_Hafta10.Resource1.pancake;
+			this.pictureBox7.Location = new System.Drawing.Point(89, 246);
+			this.pictureBox7.Name = "pictureBox7";
+			this.pictureBox7.Size = new System.Drawing.Size(123, 117);
+			this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+			this.pictureBox7.TabIndex = 15;
+			this.pictureBox7.TabStop = false;
+			this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(384, 227);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(85, 13);
+			this.label4.TabIndex = 17;
+			this.label4.Text = "Ödenecek Tutar";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(387, 261);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(0, 13);
+			this.label5.TabIndex = 18;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(369, 190);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(107, 23);
+			this.button2.TabIndex = 19;
+			this.button2.Text = "Listeden Çıkart";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(528, 374);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.pictureBox6);
+			this.Controls.Add(this.pictureBox7);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.pictureBox5);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "CafeApp";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.PictureBox pictureBox6;
+		private System.Windows.Forms.PictureBox pictureBox7;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ CafeApp-Hafta10/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CafeApp_Hafta10
+{
+	public partial class Form1 : Form
+	{
+		int toplamPara = 1000;
+		int tutar = 0;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			label2.Text = toplamPara.ToString() + " TL";
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox2_Click(object sender, EventArgs e)
+		{
+			tutar += 115;
+			label5.Text = tutar.ToString() + " TL";
+			listBox1.Items.Add("Peynir");
+		}
+
+		private void pictureBox3_Click(object sender, EventArgs e)
+		{
+			tutar += 75;
+			label5.Text = tutar.ToString() + " TL";
+			listBox1.Items.Add("Cupcake");
+		}
+
+		private void pictureBox5_Click(object sender, EventArgs e)
+		{
+			tutar += 225;
+			label5.Text = tutar.ToString() + " TL";
+			listBox1.Items.Add("Hamburger");
+		}
+
+		private void pictureBox4_Click(object sender, EventArgs e)
+		{
+			tutar += 100;
+			label5.Text = tutar.ToString() + " TL";
+			listBox1.Items.Add("Hot Dog");
+		}
+
+		private void pictureBox7_Click(object sender, EventArgs e)
+		{
+			tutar += 60;
+			label5.Text = tutar.ToString() + " TL";
+			listBox1.Items.Add("Puncake");
+		}
+
+		private void pictureBox6_Click(object sender, EventArgs e)
+		{
+			tutar += 300;
+			label5.Text = tutar.ToString() + " TL";
+			listBox1.Items.Add("Pizza");
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if(toplamPara >= tutar)
+			{
+				MessageBox.Show("Ödemeniz Alınmıştır!","Bilgilendirme", 
+					MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			else
+			{
+				MessageBox.Show("Yetersiz Bakiye!", "Bilgilendirme",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			
+			String silinecekDeger = listBox1.Items[listBox1.Items.Count - 1].ToString();
+			switch (silinecekDeger)
+			{
+				case "Pizza":
+					tutar -= 300;
+					label5.Text = tutar.ToString() + " TL";
+					break;
+				case "Puncake":
+					tutar -= 60;
+					label5.Text = tutar.ToString() + " TL";
+					break;
+				case "Hot Dog":
+					tutar -= 100;
+					label5.Text = tutar.ToString() + " TL";
+					break;
+				case "Hamburger":
+					tutar -= 225;
+					label5.Text = tutar.ToString() + " TL";
+					break;
+				case "Cupcake":
+					tutar -= 75;
+					label5.Text = tutar.ToString() + " TL";
+					break;
+				case "Peynir":
+					tutar -= 115;
+					label5.Text = tutar.ToString() + " TL";
+					break;
+			}
+			listBox1.Items.RemoveAt(listBox1.Items.Count - 1);
+		}
+	}
+}
